0f4b8f625bea3b96c65c90e9320b5533da9c84d7
jhlee8901 <jhlee8901@gmail.com>
2016-07-21 15:49:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,42 +2,36 @@
 #include <string.h>
 
 
-/* Define & Global */
+/* Define Macro */
 
-/*
-typedef struct _Register{
-unsigned short cpsr_flag, spsr_flag;
-unsigned int general_register[12];
-unsigned int cpsr, spsr;
-}Regist;
-
-Regist reg_val;
-*/
-
-#define SRAM_SRC 0x00001600 //SRAM source address
-#define SRAM_DST 0x00011600 //SRAM dump address
-#define SRAM_SIZE 64		//SRAM size 64KB
+#define SRAM_SRC 0x00001600		//SRAM source address
+#define SRAM_DST 0x00011600		//SRAM dump address
+#define SRAM_SIZE 64			//SRAM size 64KB
 
 
-#define TCM_SRC 0x00022000	//TCM source address
-#define TCM_DST 0x00023000	//TCM dump address
-#define TCM_SIZE 4			//TCM size 4KB
+#define TCM_SRC 0x00022000		//TCM source address
+#define TCM_DST 0x00023000		//TCM dump address
+#define TCM_SIZE 4				//TCM size 4KB
 
 
-#define REG_BASE_ADDR 0x00001000 //Base address
-
-extern void Dump_Regist(unsigned int reg_base_addr);
+//#define REG_BASE_ADDR 0x00001000 //Base address
 
 
 
+/* Registers Dump */
 
-/* EXCEPTION Generate CODE */
+//unsigned int base_addr = REG_BASE_ADDR; // Register dirt 문제를 막기 위해 전역으로 빼버림
 
-void(*funcP)(void);
+extern void Dump_Regist(void);
+//extern void Dump_Regist(unsigned int reg_base_addr);
+//extern void Dump_Regist_Push();
 
-#pragma push
-#pragma O0
+
+
+/* EXCEPTION Generate Code */
+
 typedef void(*_func_t)(void);
+
 static void undef(void)
 {
 	static const int instr = 0xffffffff;
@@ -46,7 +40,7 @@ static void undef(void)
 
 static void data_abort(void)
 {
-	static int * null_p = (int *)0x1;
+	static int * null_p = (int *)0x12345678;
 	int temp;
 	temp = *null_p;
 }
@@ -57,52 +51,54 @@ static void prefetch_abort(void)
 	foo = (_func_t)(0xffffffff);
 	(*foo)();
 }
-#pragma pop
 
 
-/* SRAM Memory dump code */
+/* SRAM Memory Dump */
 
 static int Dump_SRAM()
 {
-	unsigned char ret = 0;
+	unsigned char ret;
 	unsigned const char* SRAM_src_addr = SRAM_SRC;
 	unsigned char* SRAM_dst_addr = SRAM_DST;
-	
+
 	//dump
 	memcpy(SRAM_dst_addr, SRAM_src_addr, SRAM_SIZE * 1024);
-	
+
 	//check
-	ret = memcmp(SRAM_src_addr, SRAM_dst_addr, SRAM_SIZE * 1024); // 0 equal 
+	ret = memcmp(SRAM_src_addr, SRAM_dst_addr, SRAM_SIZE * 1024);
 
 	return ret;
 }
 
-/* TCM Memory dump code */
+
+
+/* TCM Memory Dump */
 
 static int Dump_TCM()
 {
-	unsigned char ret = 0;
+	unsigned char ret;
 	unsigned const char* TCM_src_addr = TCM_SRC;
 	unsigned char* TCM_dst_addr = TCM_DST;
-	
+
 	//dump
-	memcpy(TCM_dst_addr, TCM_src_addr, TCM_SIZE*1024);
-	
+	memcpy(TCM_dst_addr, TCM_src_addr, TCM_SIZE * 1024);
+
 	//check
-	ret = memcmp(TCM_src_addr, TCM_dst_addr, TCM_SIZE * 1024); // 0 equal 
-	
+	ret = memcmp(TCM_src_addr, TCM_dst_addr, TCM_SIZE * 1024);
+
 	return ret;
 }
 
 
-/* memory input test */
+
+/* Write Data for dump test */
 
 static void Input_Test()
 {
 	unsigned int* sram_base_addr = SRAM_SRC;
 	unsigned int* tcm_base_addr = TCM_SRC;
 
-	int len,i;
+	int len, i;
 
 	len = SRAM_SIZE * 1024 / 4;
 
@@ -119,90 +115,114 @@ static void Input_Test()
 
 
 
+/* main */
+
 void main(void)
 {
-	//int *bad = NULL;
-	
 
+	// write data
 
-	// input test
 	Input_Test();
 
 
-	// UNDEFINED exception TEST 1
+	// UNDEF Exception TEST
+
 	undef();
 
-	
 
-	// DATA ABORT exception TEST 1
+	// DATA ABORT Exception TEST
 
-	// a = *bad; // exception 안남	
-
-
-
-	// DATA ABORT exception TEST 2
-	
 	//data_abort();
 
 
+	// PREFETCH ABORT Exception TEST
 
-	// PREFETCH ABORT exception TEST 1
-	
 	//prefetch_abort();
 
 
-
-	// PREFETCH ABORT exception TEST 1
-
-	//funcP = (void(*)(void)) 0x00000001; //======> prefetch 안되고 undefined 로 빠짐	
-	//funcP();
-		
-	
-
-	//Dump_memory(0x0,4096);
-	
-	//printf("Dump Register\n");
-	//Dump_Regist(base_addr);
-
-	printf("No!!!!\n");
-
-	while(1)
+	while (1)
 	{
 		printf("hello world\n");
-		//*bad = 0xffff;
 	}
-	
+
 }
 
-void __irq harm_undefined_handler_dump()
-{	
-	unsigned int base_addr = REG_BASE_ADDR;
 
-	Dump_Regist(base_addr);
+
+/* ABORT(data) Exception handler */
+
+void __irq harm_abort_handler_dump()
+{
+	//unsigned int base_addr = REG_BASE_ADDR;
+
+	//Dump_Regist(base_addr);
+	Dump_Regist();
+
 	printf("[OK] Register\n");
 
 	if (!Dump_SRAM()){
 		printf("[OK] SRAM\n");
 	}
-		
+
 	if (!Dump_TCM()){
 		printf("[OK] TCM\n");
 	}
 
-	printf("UNDEFINED!\n");	
-	for (;;);
-}
-
-void __irq harm_abort_handler_dump()
-{
-	//
 	printf("DATA ABORT!\n");
 	for (;;);
 }
 
+
+
+/* UNDEF Exception handler */
+
+void __irq harm_undefined_handler_dump()
+{
+	//실질적인 Register dump 는 이 부분에서 끝내야 한다
+
+	//unsigned int base_addr = REG_BASE_ADDR; // R4 가 덮어씌워짐
+
+	//Dump_Regist_Push();
+	//Dump_Regist(base_addr); // parameter로 인해서 R0 가 덮어씌워짐
+	Dump_Regist();
+
+	printf("[OK] Register\n");
+
+	if (!Dump_SRAM()){
+		printf("[OK] SRAM\n");
+	}
+
+	if (!Dump_TCM()){
+		printf("[OK] TCM\n");
+	}
+
+	printf("UNDEF!\n");
+
+
+	for (;;);
+}
+
+
+
+/* ABORT(prefetch) Exception handler */
+
 void __irq harm_prefetch_handler_dump()
 {
-	//
+	//unsigned int base_addr = REG_BASE_ADDR;
+
+	//Dump_Regist(base_addr);
+	Dump_Regist();
+
+	printf("[OK] Register\n");
+
+	if (!Dump_SRAM()){
+		printf("[OK] SRAM\n");
+	}
+
+	if (!Dump_TCM()){
+		printf("[OK] TCM\n");
+	}
+
 	printf("PREFETCH ABORT!\n");
 	for (;;);
 }
